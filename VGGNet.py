bedeced98b25419f8f9244d9db216bc3db61efda
chunhuiLuo <keven411_127@163.com>
2018-01-08 21:22:01 +0800

    add VGG

---
+++ VGGNet.py
@@ -0,0 +1,194 @@
+import numpy as np 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+# from torch.utils.data import TensorDataset
+import torch.utils.data as data
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
+import torchvision.transforms as T
+from common.dataset import TIN200Data
+
+# from parse_data import Reader
+
+
+
+gpu_type = torch.cuda.FloatTensor
+# class Mydatasets(data.Dataset):
+#     def __init__(self, dataset, labels, transform=None, target_transform=None):
+#         self.dataset = dataset
+#         self.labels = labels
+#         self.transform = transform
+#         self.target_transform = target_transform
+
+#     def __getitem__(self, index):
+#         data, target = self.dataset[index], self.labels[index]
+#         if self.transform is not None:
+#             data = self.transform(data)
+#         if self.target_transform is not None:
+#             target = self.target_transform(target)
+#         return data, target
+
+#     def __len__(self):
+#         return len(self.dataset)
+
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        N, C, H, W = x.size()
+        return x.view(N, -1)
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.conv1 = nn.Sequential(
+                        nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+                        nn.ReLU(),
+                        nn.BatchNorm2d(num_features=64, eps=1e-06, momentum=0.9),
+                        nn.MaxPool2d(kernel_size=2, stride=2),
+                        )
+        self.conv2 = nn.Sequential(
+                        nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+                        nn.ReLU(),
+                        nn.BatchNorm2d(num_features=128, eps=1e-06, momentum=0.9),
+                        nn.MaxPool2d(kernel_size=2, stride=2),
+                        )
+        self.conv3 = nn.Sequential(
+                        nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+                        nn.ReLU(),
+                        nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+                        nn.ReLU(),
+                        nn.BatchNorm2d(num_features=256, eps=1e-06, momentum=0.9),
+                        # nn.MaxPool2d(kernel_size=2, stride=2),
+                        )
+        self.conv4 = nn.Sequential(
+                        nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+                        nn.ReLU(),
+                        nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+                        nn.ReLU(),
+                        nn.BatchNorm2d(num_features=512, eps=1e-06, momentum=0.9),
+                        # nn.MaxPool2d(kernel_size=2, stride=2),
+                        )
+        self.conv5 = nn.Sequential(
+                        nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+                        nn.ReLU(),
+                        nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+                        nn.ReLU(),
+                        nn.BatchNorm2d(num_features=512, eps=1e-06, momentum=0.9),
+                        nn.MaxPool2d(kernel_size=2, stride=2),
+                        )
+        self.fc = nn.Sequential(
+                        Flatten(),
+                        nn.Linear(8*8*512, 2048, bias=False),
+                        nn.Dropout2d(p=0.5, inplace=True),
+                        nn.Linear(2048, 1024, bias=False),
+                        nn.Dropout2d(p=0.5, inplace=True),
+                        nn.Linear(1024, 200, bias=False),
+                        nn.Softmax(),
+                        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.fc(x)
+        return x
+
+class Test_Model(nn.Module):
+    def __init__(self):
+        super(Test_Model, self).__init__()
+        self.conv = nn.Sequential(
+                        nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+                        nn.ReLU(),
+                        nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+                        nn.ReLU(),
+                        nn.MaxPool2d(kernel_size=2, stride=2),
+                        nn.Dropout2d(p=0.25),
+                        nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+                        nn.ReLU(),
+                        nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
+                        nn.ReLU(),
+                        nn.MaxPool2d(kernel_size=2, stride=2),
+                        nn.Dropout2d(p=0.25),
+                        Flatten(),
+                        nn.Linear(16*16*64, 4096),
+                        nn.ReLU(),
+                        nn.Dropout2d(p=0.5),
+                        nn.Linear(4096, 2048),
+                        nn.Linear(2048, 200),
+                        nn.Softmax(),
+                    )
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+
+def train(model, loss_fn, optimizer, num_epochs = 1, loader=None):
+    num_correct = 0
+    num_samples = 0
+    for epoch in range(num_epochs):
+        print('Starting epoch %d / %d' % (epoch + 1, num_epochs))
+        model.train()
+        for t, (x, y) in enumerate(loader):
+            x_train = Variable(x.type(gpu_type))
+            y_train = Variable(y.type(gpu_type).long())
+
+            scores = model(x_train)
+            loss = loss_fn(scores, y_train)
+
+            _, preds = scores.data.cpu().max(1)
+            num_correct += (preds == y).sum()
+            num_samples += preds.size(0)
+            acc = float(num_correct) / num_samples
+            if (t + 1) % 20 == 0:
+                print('t = %d, loss = %.4f, acc = %.4f' % (t + 1, loss.data[0], acc))
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+def check_accuracy(model, loader):
+
+    print('Checking accuracy on validation set')
+  
+    num_correct = 0
+    num_samples = 0
+    model.eval()               # Put the model in test mode (the opposite of model.train(), essentially)
+    for x, y in loader:
+        x_var = Variable(x, volatile=True)
+
+        scores = model(x_var.type(gpu_type))
+        _, preds = scores.data.cpu().max(1)
+        num_correct += (preds == y).sum()
+        num_samples += preds.size(0)
+        acc = float(num_correct) / num_samples
+        print('Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * acc))
+
+
+def main():
+    torch.cuda.is_available()
+    mytransform = T.ToTensor()
+    # train_datasets = Mydatasets(train_data, train_labels, transform=mytransform )
+    # val_datasets = Mydatasets(val_data, val_labels, transform=mytransform)
+    train_data = TIN200Data('/data1/tiny-imagenet-200', '/data1/tiny-imagenet-200/wnids.txt', data_dir='train')
+    val_data = TIN200Data('/data1/tiny-imagenet-200', '/data1/tiny-imagenet-200/wnids.txt', data_dir='val')
+
+    train_loader = DataLoader(train_data, batch_size=16, shuffle=True, num_workers=2)
+    val_loader = DataLoader(val_data, batch_size=16, shuffle=True, num_workers=2)
+
+    # model = Model()
+    model = Model().type(gpu_type)
+    optimizer = optim.SGD(params=model.parameters(), lr=0.05, momentum=0.9, weight_decay=1e-06, nesterov=True)
+    loss_fn = nn.CrossEntropyLoss()
+
+    train(model, loss_fn, optimizer, num_epochs = 3, loader=train_loader)
+    check_accuracy(model, val_loader)
+
+  
+
+if __name__ == '__main__':
+    main()
