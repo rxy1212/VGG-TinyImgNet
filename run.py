4a008c8d02cbe6b4f43f9363d6a21a040d5989af
ryan stark <ryandotstark@163.com>
2018-01-10 16:21:41 +0800

    debug

--- run.py
+++ run.py
@@ -105,8 +105,8 @@ def main():
     # test_datasets = TIN200Data(
     #     './tiny-imagenet-200', './tiny-imagenet-200/wnids.txt', 'test')
 
-    train_loader = data.DataLoader(train_datasets, batch_size=64, shuffle=True, num_workers=2)
-    val_loader = data.DataLoader(val_datasets, batch_size=64, shuffle=True, num_workers=2)
+    train_loader = data.DataLoader(train_datasets, batch_size=128, shuffle=True, num_workers=2)
+    val_loader = data.DataLoader(val_datasets, batch_size=128, shuffle=True, num_workers=2)
 
     net = VGGNet().cuda()
 
