1dbfd06598046573d6e3ce28b9af0400c8452010
worksking <1604526707@qq.com>
2018-01-12 17:12:28 +0800

    init

--- common/net.py
+++ common/net.py
@@ -9,74 +9,73 @@
 
 import torch.nn as nn
 
+
 class VGGNet(nn.Module):
     def __init__(self):
         super(VGGNet, self).__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1, dilation=1, groups=1, bias=False),
+            nn.Conv2d(3, 64, 3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(num_features=32, momentum=0.9, affine=False),
-            nn.Conv2d(32, 64, 3, 1, 1, 1, 1, False),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, 3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(num_features=64, momentum=0.9, affine=False),
-            nn.Conv2d(64, 64, 3, 1, 1, 1, 1, False),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, 3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(num_features=64, momentum=0.9, affine=False)
+            nn.BatchNorm2d(64)
         )
 
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, dilation=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.conv2 = nn.Sequential(
-            nn.Conv2d(64,128, 3, 1, 1, 1, 1, False),
+            nn.Conv2d(64, 128, 3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(num_features=128, momentum=0.9, affine=False),
-            nn.Conv2d(128, 128, 3, 1, 1, 1, 1, False),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 128, 3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(num_features=128, momentum=0.9, affine=False),
-            nn.Conv2d(128, 128, 3, 1, 1, 1, 1, False),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 128, 3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(num_features=128, momentum=0.9, affine=False)
+            nn.BatchNorm2d(128)
         )
 
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, dilation=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.conv3 = nn.Sequential(
-            nn.Conv2d(128, 256, 3, 1, 1, 1, 1, False),
+            nn.Conv2d(128, 256, 3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(num_features=256, momentum=0.9, affine=False),
-            nn.Conv2d(256, 256, 3, 1, 1, 1, 1, False),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(256, 256, 3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(num_features=256, affine=False),
-            nn.Conv2d(256, 512, 3, 1, 1, 1, 1, False),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(256, 512, 3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(num_features=512, momentum=0.9, affine=False)
+            nn.BatchNorm2d(512)
         )
 
         self.conv4 = nn.Sequential(
-            nn.Conv2d(512, 512, 3, 1, 1, 1, 1, False),
+            nn.Conv2d(256, 512, 3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(num_features=512, momentum=0.9, affine=False),
-            nn.Conv2d(512, 512, 3, 1, 1, 1, 1, False),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(512, 512, 3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(num_features=512, momentum=0.9, affine=False),
-            nn.Conv2d(512, 512, 3, 1, 1, 1, 1, False),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(512, 512, 3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(num_features=512, momentum=0.9, affine=False)
+            nn.BatchNorm2d(512)
         )
 
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2, dilation=1)
-
-        self.flatten = lambda x: x.view(x.size(0), -1)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.fcn = nn.Sequential(
-            nn.Linear(8 * 8 * 512, 4096),
+            nn.Linear(512 * 8 * 8, 4096),
             nn.ReLU(),
             nn.Dropout(),
             nn.Linear(4096, 2048),
             nn.ReLU(),
             nn.Dropout(),
-            nn.Linear(2048, 200),
-            nn.Softmax()
+            nn.Linear(2048, 200, bias=False),
+            nn.Softmax(dim=1)
         )
 
     def forward(self, x):
@@ -85,8 +84,52 @@ class VGGNet(nn.Module):
         x = self.conv2(x)
         x = self.pool2(x)
         x = self.conv3(x)
-        x = self.conv4(x)
+        # x = self.conv4(x)
         x = self.pool3(x)
-        x = self.flatten(x)
+        x = x.view(x.size(0), -1)
         x = self.fcn(x)
         return x
+
+
+CONF = {
+    # 'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'], origin
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+class VGG11(nn.Module):
+    def __init__(self):
+        super(VGG11, self).__init__()
+        self.features = self._make_layers(CONF['A'])
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 8 * 8, 200),
+            #nn.ReLU(),
+            #nn.Dropout(),
+            #nn.Linear(4096, 4096),
+            #nn.ReLU(),
+            #nn.Dropout(),
+            #nn.Linear(4096, 200)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def _make_layers(self, conf, batch_norm=False):
+        layers = []
+        in_channels = 3
+        for v in conf:
+            if v == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                 layers += [nn.Conv2d(in_channels, v, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(v),
+                           nn.ReLU(inplace=True)]
+            in_channels = v
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import torch.optim as optim
 import torch.utils.data as data
 from torch.autograd import Variable
 from common.net import VGGNet
+from common.senet import SENet18
 from common.densenet import DenseNet
 from common.dataset import TIN200Data
 from common.utils import localtime, save
@@ -21,40 +22,39 @@ import torchvision.models as models
 import torch.backends.cudnn as cudnn
 
 
-def train(model, loss_fn, optimizer, num_epochs=1, loader=None, val_loader):
+def train(model, loss_fn, optimizer, num_epochs=1, epochs = 0, loader=None):
     num_correct = 0
     num_samples = 0
-    for epoch in range(num_epochs):
-        print('Starting epoch %d / %d' % (epoch + 1, num_epochs))
-        model.train()
-        for t, (x, y) in enumerate(loader):
-            x_train = Variable(x.cuda())
-            y_train = Variable(y.cuda())
+    #for epoch in range(num_epochs):
+    print('Starting epoch %d / %d' % (epochs + 1, num_epochs))
+    model.train()
+    for t, (x, y) in enumerate(loader):
+        x_train = Variable(x.cuda())
+        y_train = Variable(y.cuda())
 
-            scores = model(x_train)
-            loss = loss_fn(scores, y_train)
+        scores = model(x_train)
+        loss = loss_fn(scores, y_train)
 
-            # reference https://discuss.pytorch.org/t/argmax-with-pytorch/1528
-            _, preds = scores.data.cpu().max(1)
+        # reference https://discuss.pytorch.org/t/argmax-with-pytorch/1528
+        _, preds = scores.data.cpu().max(1)
 
-            num_correct += (preds == y).sum()
-            num_samples += preds.size(0)
-            acc = float(num_correct) / num_samples
-            if (t + 1) % 20 == 0:
-                print('t = %d, loss = %.4f, acc = %.4f' %
-                      (t + 1, loss.data[0], acc))
+        num_correct += (preds == y).sum()
+        num_samples += preds.size(0)
+        acc = float(num_correct) / num_samples
+        if (t + 1) % 20 == 0:
+            print('t = %d, loss = %.4f, acc = %.4f' %
+                    (t + 1, loss.data[0], acc))
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        check_accuracy(model,val_loader)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
 
 def check_accuracy(model, loader):
     print('Checking accuracy on validation set')
 
-    val_correct = 0
-    val_samples = 0
+    num_correct = 0
+    num_samples = 0
     # Put the model in test mode (the opposite of model.train(), essentially)
     model.eval()
     for x, y in loader:
@@ -63,10 +63,10 @@ def check_accuracy(model, loader):
 
         scores = model(x_var.type(torch.cuda.FloatTensor))
         _, preds = scores.data.cpu().max(1)
-        val_correct += (preds == y).sum()
-        val_samples += preds.size(0)
-    acc = float(val_correct) / val_samples
-    print('Got %d / %d correct (%.2f)' % (val_correct, val_samples, 100 * acc))
+        num_correct += (preds == y).sum()
+        num_samples += preds.size(0)
+    acc = float(num_correct) / num_samples
+    print('Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * acc))
 
 
 def predict(model, loader):
@@ -93,7 +93,6 @@ def predict(model, loader):
         _, preds = scores.data.cpu().max(1)
         classid += [classid_map[p] for p in preds]
 
-
     with open(pjoin(os.getcwd(), 'predictions', localtime(), '.txt'), 'w') as f:
         for i in len(classid):
             f.write(f'{test_img_name[i]} {classid[i]}\n')
@@ -102,7 +101,7 @@ def predict(model, loader):
 def main():
     os.environ["CUDA_VISIBLE_DEVICES"] = "2,3"
     use_cuda = torch.cuda.is_available()
-    
+
     train_datasets = TIN200Data(
         '/data1/tiny-imagenet-200', '/data1/tiny-imagenet-200/wnids.txt')
     val_datasets = TIN200Data('/data1/tiny-imagenet-200',
@@ -110,14 +109,17 @@ def main():
     # test_datasets = TIN200Data(
     #     './tiny-imagenet-200', './tiny-imagenet-200/wnids.txt', 'test')
 
-    train_loader = data.DataLoader(train_datasets, batch_size=120, shuffle=True, num_workers=4)
-    val_loader = data.DataLoader(val_datasets, batch_size=120, shuffle=True, num_workers=4)
+    train_loader = data.DataLoader(
+        train_datasets, batch_size=64, shuffle=True, num_workers=4)
+    val_loader = data.DataLoader(
+        val_datasets, batch_size=120, shuffle=True, num_workers=4)
 
     #net = VGGNet()
     #net = models.resnet18()
     #net.conv1 = nn.Conv2d(3,64,kernel_size = 3,stride=1, padding=1 ,bias=False)
     #net.fc = nn.Linear(4096,200)
-    net = DenseNet(64,22,0.5,200,64)
+    net = VGGNet()
+    #net = SENet18()
     #net.cuda()
     if use_cuda:
         net.cuda()
@@ -125,14 +127,18 @@ def main():
             net, device_ids=range(torch.cuda.device_count()))
         cudnn.benchmark = True
     #optimizer = optim.SGD(params=net.parameters(), lr=7e-3, momentum=0.99, weight_decay= 5e-5, nesterov=True)
-    optimizer = optim.Adam(params=net.parameters(), lr=7e-4, weight_decay = 1e-3)
+    #optimizer = optim.Adam(params=net.parameters(), lr=5e-3, weight_decay=5e-3)
+    optimizer = optim.SGD(net.parameters(), lr=0.01,
+                          momentum=0.99, weight_decay=0.01)
 
     loss_fn = nn.CrossEntropyLoss()
-
-    train(net, loss_fn, optimizer, num_epochs=30, loader=train_loader,val_loader = val_loader)
-    #check_accuracy(net, val_loader)
+    num_epochs =30
+    for epoch in range(num_epochs):
+        train(net, loss_fn, optimizer, num_epochs=num_epochs, epochs=epoch, loader=train_loader)
+        check_accuracy(net, val_loader)
 
     save(net)
+
 
 if __name__ == '__main__':
     main()
