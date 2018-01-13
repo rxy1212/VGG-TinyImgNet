401125b87c3a605fda3aaae79bf5769bcefa14f1
ryan stark <ryandotstark@163.com>
2018-01-13 15:43:39 +0800

    test

--- common/utils.py
+++ common/utils.py
@@ -25,9 +25,9 @@ def save(net, state_dict=False, replace=False):
     '''
     if replace:
         if state_dict:
-            torch.save(net.state_dict(), f'./saved_nets/best_vgg13_state.pkl')
+            torch.save(net.state_dict(), f'./saved_nets/best_vgg16_state.pkl')
         else:
-            torch.save(net, f'./saved_nets/best_vgg13.pkl')
+            torch.save(net, f'./saved_nets/best_vgg16.pkl')
     else:
         if state_dict:
             torch.save(net.state_dict(), f'./saved_nets/net_state_{localtime()}.pkl')
