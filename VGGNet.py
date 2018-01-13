18e3e47d8a9c55c79e35b9d97932e81edd701f0f
chunhuiLuo <keven411_127@163.com>
2018-01-13 18:35:16 +0800

    VGG_19

--- VGGNet.py
+++ VGGNet.py
@@ -10,7 +10,7 @@ from torch.utils.data import sampler
 import torchvision.transforms as T
 import torch.backends.cudnn as cudnn
 from common.dataset import TIN200Data
-from common.net import VGG_19
+import common.net as net
 
 gpu_type = torch.cuda.FloatTensor
 
@@ -170,7 +170,7 @@ def main():
     val_loader = DataLoader(val_data, batch_size=128, shuffle=True, num_workers=2)
 
     # model = Model().cuda()
-    Model = VGG_19().cuda()
+    Model = net.VGG_19().cuda()
     cudnn.benchmark = True
     # model = Test_Model().cuda()
 
