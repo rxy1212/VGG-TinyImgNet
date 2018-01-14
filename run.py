67e80254222d5040551fd175f60aa33a1aed88fe
worksking <1604526707@qq.com>
2018-01-14 08:35:25 +0800

    init

--- run.py
+++ run.py
@@ -135,7 +135,7 @@ def main():
         net = torch.nn.DataParallel(
             net, device_ids=range(torch.cuda.device_count()))
         cudnn.benchmark = True
-    optimizer = optim.SGD(params=net.parameters(), lr=5e-4, momentum=0.99,eps = weight_decay= 5e-5, nesterov=True)
+    optimizer = optim.SGD(params=net.parameters(), lr=5e-4, momentum=0.99,weight_decay= 5e-5, nesterov=True)
     #optimizer = optim.Adam(params=net.parameters(), lr=7e-3, weight_decay = 4e-3)
 
     loss_fn = nn.CrossEntropyLoss()
