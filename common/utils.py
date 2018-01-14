e97a0357139c62c1c4716d302e16cba6161e9ae5
ryan stark <ryandotstark@163.com>
2018-01-14 17:37:20 +0800

    debug

--- common/utils.py
+++ common/utils.py
@@ -78,5 +78,5 @@ def adjust_learning_rate(optimizer, decay_rate=0.95):
     '''
     Use learning rate decay
     '''
-    for param_group in optimizer.param_group:
+    for param_group in optimizer.param_groups:
         param_group['lr'] = param_group['lr'] * decay_rate
