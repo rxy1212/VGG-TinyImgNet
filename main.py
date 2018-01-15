4429ae66b475f247ef67b9ac553656382a96815c
worksking <1604526707@qq.com>
2018-01-16 03:27:04 +0800

    init

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ def adjust_learning_rate(optimizer, num_epoch):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
     #lr = args.lr * (0.1 ** (epoch // 30))
     for param_group in optimizer.param_groups:
-        param_group['lr'] = param_group['lr'] * (0.1 ** (num_epoch // 30))
+        param_group['lr'] = param_group['lr'] * (0.9 ** (num_epoch // 10))
 
 
 def main():
