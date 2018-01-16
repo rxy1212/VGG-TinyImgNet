5f0e2f28d2c8930c7bed7a41e322194745387b4a
worksking <1604526707@qq.com>
2018-01-16 10:10:18 +0800

    init

--- run.py
+++ run.py
@@ -139,7 +139,7 @@ def main():
     #net = models.resnet18()
     #net.conv1 = nn.Conv2d(3,64,kernel_size = 3,stride=1, padding=1 ,bias=False)
     #net.fc = nn.Linear(4096,200)
-    net = DenseNet(32,16,0.5,200,64)
+    net = DenseNet(48,16,0.5,200,64)
     #net.cuda()
     if use_cuda:
         net.cuda()
