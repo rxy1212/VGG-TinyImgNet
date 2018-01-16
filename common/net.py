f83f14b61d766401ee53b38da64f470ec6788e83
ryan stark <ryandotstark@163.com>
2018-01-16 14:20:47 +0800

    debug

--- common/net.py
+++ common/net.py
@@ -9,9 +9,9 @@
 
 import torch.nn as nn
 
-class VGGNet(nn.Module):
+class VGGNet4(nn.Module):
     def __init__(self):
-        super(VGGNet, self).__init__()
+        super(VGGNet4, self).__init__()
         self.conv1 = nn.Sequential(
             nn.Conv2d(3, 64, 3, padding=1),
             nn.ReLU(),
@@ -83,6 +83,73 @@ class VGGNet(nn.Module):
         x = self.pool2(x)
         x = self.conv3(x)
         x = self.conv4(x)
+        x = self.pool3(x)
+        x = x.view(x.size(0), -1)
+        x = self.fcn(x)
+        return x
+
+
+class VGGNet3(nn.Module):
+    def __init__(self):
+        super(VGGNet3, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64)
+        )
+
+        self.pool1 = nn.MaxPool2d(kernel_size=4, stride=2, padding=1)
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 128, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 128, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128)
+        )
+
+        self.pool2 = nn.MaxPool2d(kernel_size=4, stride=2, padding=1)
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(512)
+        )
+
+        self.pool3 = nn.MaxPool2d(kernel_size=4, stride=2, padding=1)
+
+        self.fcn = nn.Sequential(
+            nn.Linear(512 * 8 * 8, 4096),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(4096, 2048),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(2048, 200, bias=False)
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
+        x = self.conv3(x)
         x = self.pool3(x)
         x = x.view(x.size(0), -1)
         x = self.fcn(x)
--- run.py
+++ run.py
@@ -14,7 +14,7 @@ import torch.optim as optim
 import torch.utils.data as data
 from torch.autograd import Variable
 import torch.backends.cudnn as cudnn
-from common.net import VGGNet
+from common.net import VGGNet3
 from common.dataset import TIN200Data
 from common.utils import *
 
@@ -51,7 +51,7 @@ def train(net, loss_fn, optimizer, scheduler, num_epochs=1, loader=None, val_loa
         if acc > best_acc:
             best_acc = acc
             print(f'Got current best_acc:{best_acc:.2f}%, Saving...')
-            save(net, 'vggnet')
+            save(net, 'vggnet3')
         current_lr = optimizer.param_groups[0]['lr']
         print(f'current lr:{current_lr}')
         # adjust_learning_rate(optimizer, decay_rate=0.9)
@@ -100,12 +100,11 @@ def main(flag=True):
         train_loader = data.DataLoader(train_datasets, batch_size=200, shuffle=True, num_workers=4)
         val_loader = data.DataLoader(val_datasets, batch_size=200, num_workers=4)
 
-        net = VGGNet().cuda()
+        net = VGGNet3().cuda()
         cudnn.benchmark = True
 
         optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
-        # optimizer = optim.Adam(net.parameters(), lr=0.1)
-        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', verbose=True)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', verbose=True, patience=5)
         loss_fn = nn.CrossEntropyLoss()
 
         train(net, loss_fn, optimizer, scheduler, num_epochs=300,
