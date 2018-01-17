2f22f6d4659acb1aa4d2d798b849f4380cbe9144
worksking <1604526707@qq.com>
2018-01-17 17:57:12 +0800

    init

--- run.py
+++ run.py
@@ -154,7 +154,7 @@ def main():
         net = torch.nn.DataParallel(
             net, device_ids=range(torch.cuda.device_count()))
         cudnn.benchmark = True
-    optimizer = optim.SGD(params=net.parameters(), lr=0.1, momentum=0.9,weight_decay= 1e-4, nesterov=True)
+    optimizer = optim.SGD(params=net.parameters(), lr=0.001, momentum=0.9,weight_decay= 1e-4, nesterov=True)
     #optimizer = optim.Adam(params=net.parameters(), lr=7e-3, weight_decay = 4e-3)
     lr_schedule = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max',factor=0.2, verbose= True,patience=5)
 
