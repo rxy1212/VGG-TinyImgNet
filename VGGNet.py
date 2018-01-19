85deb842d55f257d5f9f2cc5f2db6cc92f54271f
chunhuiLuo <keven411_127@163.com>
2018-01-19 22:01:24 +0800

    ..

--- VGGNet.py
+++ VGGNet.py
@@ -176,17 +176,17 @@ def main():
     # model = Vgg19().cuda()     # net model in the net.py
     # model = Vgg11().cuda()
     model = GoogleNet().cuda()
-    model.apply(weights_init) 
+    # model.apply(weights_init) 
     cudnn.benchmark = True
 
     # model.load_state_dict(torch.load('./net_params/VGG11_net_params.pkl'))
-    optimizer = optim.SGD(params=model.parameters(), lr=0.02, momentum=0.9, weight_decay=1e-06, nesterov=True)
+    optimizer = optim.SGD(params=model.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-06, nesterov=True)
     # optimizer = optim.Adam(params=model.parameters(), lr=0.05, eps=1e-08, weight_decay=1e-05)
     loss_fn = nn.CrossEntropyLoss()
 
     scheduler = ReduceLROnPlateau(optimizer, mode='max', patience=2, verbose=True)
     best_acc = 0
-    num_epochs = 50
+    num_epochs = 30
     for epoch in range(num_epochs):
         print('Starting epoch %d / %d' % (epoch + 1, num_epochs))
         train(model, loss_fn, optimizer, loader=train_loader)
