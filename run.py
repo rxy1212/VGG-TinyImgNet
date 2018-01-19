6ab82b8bde07979dedf2ed891b1e99aa1de10193
worksking <1604526707@qq.com>
2018-01-20 01:48:34 +0800

    init

--- run.py
+++ run.py
@@ -131,7 +131,7 @@ def adjust_learning_rate(optimizer, num_epoch):
 
 
 def main():
-    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1,2"
     use_cuda = torch.cuda.is_available()
     
     train_datasets = TIN200Data('/data1')
@@ -148,8 +148,8 @@ def main():
     #net.conv1 = nn.Conv2d(3,64,kernel_size = 3,stride=1, padding=1 ,bias=False)
     #net.fc = nn.Linear(4096,200)
     #net = DenseNet(32,28,0.5,200)
-    net = DenseNet(growth_rate=64,block_config=(12, 12, 24, 18))
-    #net = densenet161()
+    #net = DenseNet(growth_rate=64,block_config=(12, 12, 24, 18))
+    net = densenet161()
     #net = densenet169()
     #net = densenet121()
     #net = densenet201()
@@ -159,7 +159,7 @@ def main():
         net = torch.nn.DataParallel(
             net, device_ids=range(torch.cuda.device_count()))
         cudnn.benchmark = True
-    optimizer = optim.SGD(params=net.parameters(), lr=0.1, momentum=0.9,weight_decay= 0, nesterov=True)
+    optimizer = optim.SGD(params=net.parameters(), lr=0.1, momentum=0.9,weight_decay= 1e-4, nesterov=True)
     #optimizer = optim.Adam(params=net.parameters(), lr=7e-3, weight_decay = 4e-3)
     lr_schedule = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max',factor=0.4, verbose= True,patience=5)
 
