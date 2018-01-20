fc1658872a74456f388478c36d52e5bc1057a942
worksking <1604526707@qq.com>
2018-01-20 16:52:33 +0800

    init

--- common/densenet161.py
+++ common/densenet161.py
@@ -159,12 +159,12 @@ class DenseNet(nn.Module):
         self.features.add_module('norm5', nn.BatchNorm2d(num_features))
 
         # Linear layer
-        self.classifier = nn.Linear(num_features*4*4, num_classes)
+        self.classifier = nn.Linear(num_features*2*2, num_classes)
 
     def forward(self, x):
         features = self.features(x)
         out = F.relu(features, inplace=True)
-        out = F.max_pool2d(out, kernel_size=3, stride=1, padding =1).view(features.size(0), -1)
+        out = F.max_pool2d(out, kernel_size=3, stride=2, padding =1).view(features.size(0), -1)
         #out = out.view(features.size(0), -1)
         out = self.classifier(out)
         return out
