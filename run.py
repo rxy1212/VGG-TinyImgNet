2e6fa6cda81b6184d83964021581b7cb788f3621
worksking <1604526707@qq.com>
2018-01-21 04:08:35 +0800

    init

--- run.py
+++ run.py
@@ -131,7 +131,7 @@ def adjust_learning_rate(optimizer, num_epoch):
 
 
 def main():
-    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "3"
     use_cuda = torch.cuda.is_available()
     
     train_datasets = TIN200Data('/data1')
@@ -140,8 +140,8 @@ def main():
     # test_datasets = TIN200Data(
     #     './tiny-imagenet-200', './tiny-imagenet-200/wnids.txt', 'test')
 
-    train_loader = data.DataLoader(train_datasets, batch_size=512, shuffle=True, num_workers=4)
-    val_loader = data.DataLoader(val_datasets, batch_size=512, shuffle=True, num_workers=4)
+    train_loader = data.DataLoader(train_datasets, batch_size=256, shuffle=True, num_workers=4)
+    val_loader = data.DataLoader(val_datasets, batch_size=256, shuffle=True, num_workers=4)
 
     #net = VGGNet()
     #net = models.resnet18()
@@ -159,7 +159,7 @@ def main():
         net = torch.nn.DataParallel(
             net, device_ids=range(torch.cuda.device_count()))
         cudnn.benchmark = True
-    optimizer = optim.SGD(params=net.parameters(), lr=0.1, momentum=0.9,weight_decay= 5e-3, nesterov=True)
+    optimizer = optim.SGD(params=net.parameters(), lr=0.01, momentum=0.9,weight_decay= 5e-3, nesterov=True)
     #optimizer = optim.Adam(params=net.parameters(), lr=7e-3, weight_decay = 4e-3)
     lr_schedule = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max',factor=0.1, verbose= True,patience=5)
 
