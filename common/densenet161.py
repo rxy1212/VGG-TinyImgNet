e955fa346188016084e76998a36c55132de9d318
worksking <1604526707@qq.com>
2018-01-21 00:18:57 +0800

    init

--- common/densenet161.py
+++ common/densenet161.py
@@ -129,7 +129,7 @@ class DenseNet(nn.Module):
     """
 
     def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
-                 num_init_features=64, bn_size=4, drop_rate=0, num_classes=200):
+                 num_init_features=64, bn_size=4, drop_rate=0.5, num_classes=200):
 
         super(DenseNet, self).__init__()
 
