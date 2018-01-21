e4e9bc5b90bd4a759042440b5f2b73dfc2ab03a8
worksking <1604526707@qq.com>
2018-01-22 02:40:15 +0800

    init

--- common/densenet161.py
+++ common/densenet161.py
@@ -167,7 +167,7 @@ class DenseNet(nn.Module):
     def forward(self, x):
         features = self.features(x)
         out = F.relu(features, inplace=True)
-        out = F.avg_pool2d(out, kernel_size=3, stride=2, padding=1).view(features.size(0), -1)
+        out = F.avg_pool2d(out, kernel_size=3, stride=1, padding=1).view(features.size(0), -1)
         #out = out.view(features.size(0), -1)
         out = self.classifier(out)
         return out
