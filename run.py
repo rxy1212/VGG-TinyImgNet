4357c0d83c5152d171038cc973a5b58b77263e11
worksking <1604526707@qq.com>
2018-01-23 13:12:28 +0800

    init

--- run.py
+++ run.py
@@ -131,7 +131,7 @@ def adjust_learning_rate(optimizer, num_epoch):
 
 
 def main():
-    os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
     use_cuda = torch.cuda.is_available()
     
     train_datasets = TIN200Data('/home/zgj/Tiny/')
@@ -140,8 +140,8 @@ def main():
     # test_datasets = TIN200Data(
     #     './tiny-imagenet-200', './tiny-imagenet-200/wnids.txt', 'test')
 
-    train_loader = data.DataLoader(train_datasets, batch_size=500, shuffle=True, num_workers=4)
-    val_loader = data.DataLoader(val_datasets, batch_size=500, shuffle=True, num_workers=4)
+    train_loader = data.DataLoader(train_datasets, batch_size=256, shuffle=True, num_workers=4)
+    val_loader = data.DataLoader(val_datasets, batch_size=256, shuffle=True, num_workers=4)
 
     #net = VGGNet()
     #net = models.resnet18()
