f0cd6094ba2aafe70928bf4a8b7417e938c7f37f
worksking <1604526707@qq.com>
2018-04-18 03:21:25 +0800

    test

--- common/densenet.py
+++ common/densenet.py
@@ -99,9 +99,9 @@ class DenseNet(nn.Module):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.weight.data.normal_(1,0.02)
             elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
+                m.weight.data.normal_(1,0.02)
                 m.bias.data.zero_()
             elif isinstance(m, nn.Linear):
                 m.bias.data.zero_()
